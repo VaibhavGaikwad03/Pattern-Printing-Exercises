7554ff610aaa563af14e7eb46db3cfea1a4381fa
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-23 22:55:11 +0530

    Add files via upload

---
+++ Pattern_Print_9.c
@@ -0,0 +1,50 @@
+/*
+Accept the number from the user and display below pattern
+
+*   *   *   *   *   *
+*   #   #   #   *   *
+*   #   #   *   $   *
+*   #   *   $   $   *
+*   *   $   $   $   *
+*   *   *   *   *   *
+
+*/
+
+#include<stdio.h>
+
+void PatternPrint(int iValue)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iValue; i++)
+    {
+        for(j = iValue-1; j >= 0; j--)
+        {
+            if((i-1 == j) || (i == 1) || (i == iValue) || (j == iValue-1) || (j == 0))
+            {
+                printf("*\t");
+            }
+            else if(i-1 < j)
+            {
+                printf("#\t");
+            }
+            else if(i > j)
+            {
+                printf("$\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iNo = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d",&iNo);
+
+    PatternPrint(iNo);
+
+    return 0;
+}
